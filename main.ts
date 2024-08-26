663d9527dc8ff3b6353bf19f4038130289226359
Tim Roddick-Hull <tim@roddickhull.com>
2024-08-26 16:12:54 +0100

    add stat block code

--- main.ts
+++ main.ts
@@ -1,134 +1,350 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Editor, MarkdownView, Plugin } from "obsidian";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface SavingThrow {
+    ability: string;
+    modifier: number;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+interface Skill {
+    skill: string;
+    modifier: number;
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+interface AbilityOrAction {
+    title: string;
+    description: string;
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+interface AbilityScores {
+    strength: number;
+    dexterity: number;
+    constitution: number;
+    intelligence: number;
+    wisdom: number;
+    charisma: number;
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+interface StatBlock {
+    name: string;
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+    size?: string;
+    creatureType?: string;
+    alignment?: string;
 
-	display(): void {
-		const {containerEl} = this;
+    ac: number;
+    hp: number;
+    speed: string;
+    challenge: string;
 
-		containerEl.empty();
+    abilityScores: AbilityScores;
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    proficiency?: number;
+
+    savingThrows: SavingThrow[];
+    skills: Skill[];
+
+    vulnerabilities?: string;
+    resistances?: string;
+    damageImmunities?: string;
+    conditionImmunities?: string;
+
+    senses?: string;
+    languages?: string;
+
+    abilities: AbilityOrAction[];
+    actions: AbilityOrAction[];
+    reactions: AbilityOrAction[];
+    bonusActions: AbilityOrAction[];
+    legendaryActions: AbilityOrAction[];
+    lairActions: AbilityOrAction[];
 }
+
+export default class DMToolsPlugin extends Plugin {
+    async onload() {
+        this.registerMarkdownCodeBlockProcessor("statblock", (source, el, ctx) => {
+            const statblockOuterWrapper = el.createDiv({cls: "dm-tools-statblock"});
+            const statblockWrapper = statblockOuterWrapper.createEl("details", {cls: "dm-tools-statblock-inner", attr: {"open": true}});
+
+            const statBlockSpec: StatBlock = JSON.parse(source); 
+
+            const titleWrapper = statblockWrapper.createEl("summary").createSpan();
+            const title = titleWrapper.createEl("div", {cls: "dm-tools-statblock-title-header", text: statBlockSpec["name"]});
+            
+            const subheading = statblockWrapper.createEl(
+                "div", 
+                {cls: "dm-tools-statblock-type-alignment", text: this.parseCreatureTypeAndAlignment(statBlockSpec)}
+            );
+
+            const keyStatSection = statblockWrapper.createDiv({cls: ["dm-tools-statblock-keystats", "dm-tools-statblock-section"]});
+            this.createPrimaryTitleAndDescription(keyStatSection, "dm-tools-statblock-keystats-ac", "Armor Class", statBlockSpec.ac.toString());
+            this.createPrimaryTitleAndDescription(keyStatSection, "dm-tools-statblock-keystats-hp", "Hit Points", statBlockSpec.hp.toString());
+            this.createPrimaryTitleAndDescription(keyStatSection, "dm-tools-statblock-keystats-speed", "Speed", statBlockSpec.speed);
+            this.createPrimaryTitleAndDescription(keyStatSection, "dm-tools-statblock-keystats-challenge", "Challenge", statBlockSpec.challenge.toString());
+            if (statBlockSpec.proficiency != undefined) {
+                this.createPrimaryTitleAndDescription(keyStatSection, "dm-tools-statblock-keystats-proficiency", "Proficiency", this.formatModifier(statBlockSpec.proficiency));
+            }
+            
+            this.formatAbilityScores(statBlockSpec, statblockWrapper);
+
+            const secondaryStatSection = statblockWrapper.createDiv({cls: ["dm-tools-statblock-secondarystats", "dm-tools-statblock-section"]})
+            this.formatSkills(statBlockSpec, secondaryStatSection);
+            this.createPrimaryTitleAndDescription(secondaryStatSection, "dm-tools-statblock-secondarystats-vulns", "Vulnerabilities", statBlockSpec.vulnerabilities);
+            this.createPrimaryTitleAndDescription(secondaryStatSection, "dm-tools-statblock-secondarystats-resistances", "Resistances", statBlockSpec.resistances);
+            this.createPrimaryTitleAndDescription(secondaryStatSection, "dm-tools-statblock-secondarystats-damage-immunities", "Damage Immunities", statBlockSpec.damageImmunities);
+            this.createPrimaryTitleAndDescription(secondaryStatSection, "dm-tools-statblock-secondarystats-condition-immunities", "Condition Immunities", statBlockSpec.conditionImmunities);
+            this.createPrimaryTitleAndDescription(secondaryStatSection, "dm-tools-statblock-secondarystats-senses", "Senses", statBlockSpec.senses);
+            this.createPrimaryTitleAndDescription(secondaryStatSection, "dm-tools-statblock-secondarystats-languages", "Languages", statBlockSpec.languages);
+            
+            this.formatAbilityOrActionList(statBlockSpec.abilities, statblockWrapper, "dm-tools-statblock-abilities");
+            this.formatAbilityOrActionList(statBlockSpec.actions, statblockWrapper, "dm-tools-statblock-actions", "ACTIONS");
+            this.formatAbilityOrActionList(statBlockSpec.reactions, statblockWrapper, "dm-tools-statblock-reactions", "REACTIONS");
+            this.formatAbilityOrActionList(statBlockSpec.bonusActions, statblockWrapper, "dm-tools-statblock-bonusactions", "BONUS ACTIONS");
+            this.formatAbilityOrActionList(statBlockSpec.legendaryActions, statblockWrapper, "dm-tools-statblock-legendaryactions", "LEGENDARY ACTIONS");
+            this.formatAbilityOrActionList(statBlockSpec.lairActions, statblockWrapper, "dm-tools-statblock-lairactions", "LAIR ACTIONS");
+        });
+
+        this.addCommand({
+            id: "add-creature-statblock",
+            name: "Add Creature Statblock",
+            editorCallback: (editor: Editor, view: MarkdownView) => {
+                editor.replaceRange(sampleStatblock, editor.getCursor())
+            },
+        })
+    }
+
+    parseCreatureTypeAndAlignment(spec: StatBlock): string {
+        const sizeAndType = ((spec.size ?? "") + " " + (spec.creatureType ?? "")).trim();
+        let components = [sizeAndType];
+        if (spec.alignment !== null) {
+            components.push(spec.alignment!);
+        }
+        return titleCase(components.join(", ")) ?? "";
+    }
+
+    formatAbilityScores(spec: StatBlock, parent: HTMLElement) {
+        const wrapper = parent.createDiv({cls: "dm-tools-statblock-abilityscores"});
+        const table = wrapper.createEl("div", {cls: "dm-tools-statblock-abilityscores-table"});
+
+        // Need a double iteration here sadly to get all the key names correctly added, then all the values, so the table formats nicely
+        for (var key in spec.abilityScores) {
+            if (spec.abilityScores.hasOwnProperty(key)) {
+                table.createEl("div", {
+                    cls: "dm-tools-statblock-abilityscores-table-header-cell",
+                    text: titleCase(key.slice(0, 3))!
+                })
+            }
+        }
+        for (var key in spec.abilityScores) {
+            if (spec.abilityScores.hasOwnProperty(key)) {
+                table.createEl("div", {
+                    cls: "dm-tools-statblock-abilityscores-table-value-cell",
+                    text: (spec.abilityScores as any)[key].toString()
+                })
+            }
+        }
+    }
+
+    formatModifier(modifier: number): string {
+        const modifierPrefix = modifier > 0 ? "+" : "";
+        return modifierPrefix + modifier.toString();
+    }
+
+    formatSkills(spec: StatBlock, parent: HTMLElement) {
+        if (spec.skills.length == 0) {
+            return;
+        }
+        var skillString = spec.skills.map((skill: Skill) => {
+            return titleCase(skill.skill) + " " + this.formatModifier(skill.modifier);
+        }).join(", ")
+        this.createPrimaryTitleAndDescription(parent, "dm-tools-statblock-secondarystats-skills", "Skills", skillString);
+    }
+
+    formatAbilityOrActionList(list: AbilityOrAction[], parent: HTMLElement, entryClass: string, title?: string) {
+        if (list.length == 0) {
+            return;
+        }
+        const abilitySection = parent.createDiv({cls: [entryClass, "dm-tools-statblock-section"]})
+        if (title != undefined) {
+            abilitySection.createDiv({cls: "dm-tools-statblock-ability-section-header", text: title.toUpperCase()})
+        }
+        list.forEach((ability: AbilityOrAction) => {
+            this.createSecondaryTitleAndDescription(abilitySection, "dm-tools-statblock-ability-item", ability.title, ability.description)
+        })
+    }
+
+    createPrimaryTitleAndDescription(parentElement: HTMLElement, parentClass: string, title: string, description: string | undefined) {
+        if (description == undefined || description.length == 0) {
+            return;
+        }
+        this.createTitledText(
+            parentElement,
+            parentClass,
+            title,
+            "dm-tools-statblock-entry-title",
+            description
+        )
+    }
+
+    createSecondaryTitleAndDescription(parentElement: HTMLElement, parentClass: string, title: string, description: string | undefined) {
+        if (description == undefined || description.length == 0) {
+            return;
+        }
+        this.createTitledText(
+            parentElement,
+            parentClass,
+            title,
+            "dm-tools-statblock-subentry-title",
+            description
+        )
+    }
+
+    createTitledText(parent: HTMLElement, parentClass: string, text1: string, class1: string, text2: string): HTMLElement {
+        const wrapperDiv = parent.createDiv({cls: parentClass});
+        wrapperDiv.createSpan({text: text1, cls: class1});
+        wrapperDiv.createSpan({text: " " + text2});
+        return wrapperDiv;
+    }
+
+    onunload() { }
+}
+
+// Modified from https://stackoverflow.com/questions/32589197/how-can-i-capitalize-the-first-letter-of-each-word-in-a-string-using-javascript
+function titleCase(str?: string): string | null {
+    if (str == null) {
+        return null;
+    }
+    var splitStr = str.toLowerCase().split(' ');
+    for (var i = 0; i < splitStr.length; i++) {
+        // You do not need to check if i is larger than splitStr length, as your for does that for you
+        // Assign it back to the array
+        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+    }
+    // Directly return the joined string
+    return splitStr.join(' '); 
+ }
+
+const sampleStatblock = `
+\`\`\`statblock
+{
+    "name": "My First Creature",
+    "size": "medium",
+    "creatureType": "undead",
+    "alignment": "neutral evil",
+    "ac": 15,
+    "hp": 45,
+    "speed": "30 ft., fly 40 ft.,
+    "abilityScores" : {
+        "strength": 10,
+        "dexterity": 10,
+        "constitution": 10,
+        "intelligence": 10,
+        "wisdom":  10,
+        "charisma": 10,
+    },
+    "savingThrows": [
+        {
+            "ability": "Dexterity",
+            "modifier": 11
+        }
+    ],
+    "skills": [
+        {
+            "skill": "Acrobatics",
+            "modifier": 10
+        }
+    ],
+    "vulnerabilities": "Bludgeoning",
+    "resistances": "Piercing",
+    "damageImmunities": "",
+    "conditionImmunities": "",
+    "senses": "",
+    "languages": "",
+    "challenge": "1/4",
+    "proficiency": 1,
+    "abilities": [
+        {
+            "title": "Some Ability",
+            "description": "This is a cool ability"
+        }
+    ],
+    "actions": [
+        {
+            "title": "Some Ability",
+            "description": "This is a cool ability"
+        }
+    ],
+    "reactions": [
+        {
+            "title": "Some Ability",
+            "description": "This is a cool ability"
+        }
+    ],
+    "bonusActions": [
+        {
+            "title": "Some Ability",
+            "description": "This is a cool ability"
+        }
+    ],
+    "legendaryActions": [
+        {
+            "title": "Some Ability",
+            "description": "This is a cool ability"
+        }
+    ],
+    "lairActions": [
+        {
+            "title": "Some Ability",
+            "description": "This is a cool ability"
+        }
+    ]
+}
+\`\`\`
+`;
+
+/*
+Sample StatBlock
+
+
+*/
+
+// class SampleModal extends Modal {
+// 	constructor(app: App) {
+// 		super(app);
+// 	}
+
+// 	onOpen() {
+// 		const {contentEl} = this;
+// 		contentEl.setText('Woah!');
+// 	}
+
+// 	onClose() {
+// 		const {contentEl} = this;
+// 		contentEl.empty();
+// 	}
+// }
+
+// class SampleSettingTab extends PluginSettingTab {
+// 	plugin: MyPlugin;
+
+// 	constructor(app: App, plugin: MyPlugin) {
+// 		super(app, plugin);
+// 		this.plugin = plugin;
+// 	}
+
+// 	display(): void {
+// 		const {containerEl} = this;
+
+// 		containerEl.empty();
+
+// 		new Setting(containerEl)
+// 			.setName('Setting #1')
+// 			.setDesc('It\'s a secret')
+// 			.addText(text => text
+// 				.setPlaceholder('Enter your secret')
+// 				.setValue(this.plugin.settings.mySetting)
+// 				.onChange(async (value) => {
+// 					this.plugin.settings.mySetting = value;
+// 					await this.plugin.saveSettings();
+// 				}));
+// 	}
+// }
